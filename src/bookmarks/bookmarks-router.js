425b780661879b232ba1389d067059d8d9f373d6
JTLiner925 <[jtliner925@gmail.com]>
2020-05-19 16:37:20 -0500

    finished exercise

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,62 +1,81 @@
-const express = require("express");
-const { v4: uuid } = require('uuid');
-const logger = require('../logger');
-const { bookmark } = require('../store');
+const express = require('express')
+const { v4: uuid } = require('uuid')
+const { isWebUri } = require('valid-url')
+const logger = require('../logger')
+const store = require('../store')
 
-const bookmarksRouter = express.Router();
-const bodyParser = express.json();
+const bookmarksRouter = express.Router()
+const bodyParser = express.json()
 
 bookmarksRouter
   .route('/bookmarks')
   .get((req, res) => {
-    res.json(bookmark);
+    res.json(store.bookmarks)
   })
   .post(bodyParser, (req, res) => {
-    const { title, url, description, rating} = req.body;
-    if (!title) {
-      logger.error(`Title is required`);
-      return res.status(400).send("Invalid data");
+    for (const field of ['title', 'url', 'rating']) {
+      if (!req.body[field]) {
+        logger.error(`${field} is required`)
+        return res.status(400).send(`'${field}' is required`)
+      }
+    }
+    const { title, url, description, rating } = req.body
+
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+      logger.error(`Invalid rating '${rating}' supplied`)
+      return res.status(400).send(`'rating' must be a number between 0 and 5`)
     }
 
-    if (!url) {
-      logger.error(`url is required`);
-      return res.status(400).send("Invalid data");
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url '${url}' supplied`)
+      return res.status(400).send(`'url' must be a valid URL`)
     }
 
-    if (!description) {
-      logger.error(`Description is required`);
-      return res.status(400).send("Invalid data");
-    }
+    const bookmark = { id: uuid(), title, url, description, rating }
 
-    if (!rating) {
-      logger.error(`Rating is required`);
-      return res.status(400).send("Invalid data");
-    }
+    store.bookmarks.push(bookmark)
 
-    const id = uuid();
-
-    const bookmarks = {
-      id,
-      title,
-      url,
-      description,
-      rating
-    };
-
-    logger.info(`Bookmark with id ${id} created`);
-
+    logger.info(`Bookmark with id ${bookmark.id} created`)
     res
       .status(201)
-      .location(`http://localhost:8000/bookmarks/${id}`)
-      .json(bookmarks)
+      .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+      .json(bookmark)
   })
 
-  bookmarksRouter
-    .route('/bookmarks/:id')
-    .get((req, res) => {
+bookmarksRouter
+  .route('/bookmarks/:bookmark_id')
+  .get((req, res) => {
+    const { bookmark_id } = req.params
 
-    })
-    .delete((req, res) => {
+    const bookmark = store.bookmarks.find(c => c.id == bookmark_id)
 
-    })
-    module.exports = bookmarksRouter;
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${bookmark_id} not found.`)
+      return res
+        .status(404)
+        .send('Bookmark Not Found')
+    }
+
+    res.json(bookmark)
+  })
+  .delete((req, res) => {
+    const { bookmark_id } = req.params
+
+    const bookmarkIndex = store.bookmarks.findIndex(b => b.id === bookmark_id)
+
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${bookmark_id} not found.`)
+      return res
+        .status(404)
+        .send('Bookmark Not Found')
+    }
+
+    store.bookmarks.splice(bookmarkIndex, 1)
+
+    logger.info(`Bookmark with id ${bookmark_id} deleted.`)
+    res
+      .status(204)
+      .end()
+  })
+
+module.exports = bookmarksRouter
--- src/store.js
+++ src/store.js
@@ -1,9 +1,21 @@
-const bookmark =[{
-  id: 1,
-  title: 'AOL',
-  url: 'http://www.aol.com',
-  description: 'really old website',
-  rating: 3
-}]
+const { v4: uuid } = require('uuid')
 
-module.exports = { bookmark }
+const bookmarks = [
+  { id: uuid(),
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
+    rating: 5 },
+  { id: uuid(),
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4 },
+  { id: uuid(),
+    title: 'MDN',
+    url: 'https://developer.mozilla.org',
+    description: 'The only place to find web documentation',
+    rating: 5 },
+]
+
+module.exports = { bookmarks }
